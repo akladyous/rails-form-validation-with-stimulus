a91e5a63330cf382f811e744c9ce9f33f5f96dca
Boula Akladyous <akladyous@gmail.com>
2022-09-20 19:14:23 -0400

    added custom errors validation to form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def feedback_for(object, attribute)
-    return nil unless object.erros.any?
+    return nil unless object.errors.any?
     content_tag :div, nil, class: %w[d-block invalid-feedback] do
-      object.errors.full_messages_for(attribute).sentence
+      object.errors.full_messages_for(attribute).to_sentence
     end
   end
 end
