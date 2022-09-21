933bd559c5216aba7b35d803ea29af08bc5b88fd
Boula Akladyous <akladyous@gmail.com>
2022-09-21 13:36:48 -0400

    added validation to age column

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
 
   validates :first_name, :last_name, :age, presence: true
-  validates :age, numericality: { only_integer: true, in: 18..65, message: 'age must be between 8 and 65' }
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 18, less_than_or_equal_to: 65 }
 
   before_create ProfileCallback
 
